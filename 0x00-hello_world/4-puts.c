1867bd9dd3066465be7544381ae1c435c4e67b2a
MenahYoung <MenahYoung@example.com>
2022-09-11 12:30:31 -0700

    hello class

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print the sring in the put function 
+*
+* Description: using the main function
+* this program prints "programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+        puts("\"programming is like building a multilingual puzzel");
+        return (0);
+}
