cae0401e016190e3dfdc281434f3775a510cce1c
MenahYoung <MenahYoung@example.com>
2022-09-14 17:06:58 -0700

    nested loop

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,18 @@
 #include "main.h"
+
 /**
- * print_alphabet - abc
- *
- * Return: 0
+ * print_alphabet - check the code for Holberton School students.
+ * Description: Prints the alphabet with _putcha
+ * Return: Always 0 for success
  */
+
 void print_alphabet(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
-
 		_putchar(c);
 	}
-	
-_putchar ('\n';
+	_putchar('\n');
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,13 @@
-#ifndef HOLBERTON_H_INCLUDED
-#defined HOLBERTON_H_INCLUDED
-
-int _putchar(char);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
+int _abs(int x);
+int print_last_digit(int x);
 void jack_bauer(void);
-void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
-void print_times-table(int n);
-void num(int r, int n);
-
+void times_table(void);
+void print_times_table(int n);
